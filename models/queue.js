a8bb646f864cfa51684bd2c242c675e9cb8cdb48
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2017-08-21 16:27:20 -0400

    added a mongodb connection file

--- models/queue.js
+++ models/queue.js
@@ -7,6 +7,7 @@ function getAllQueuedStudents() {
       db.student-q.find({}, (findErr, res) => {
         if(findErr) {
           console.log('error retrieving studentqueue');
+          db.close();
         } else {
           console.log(res);
           db.close();
@@ -15,16 +16,49 @@ function getAllQueuedStudents() {
       })
     })
     .catch( err => {
-      console.log('failed to connect to DB');
+      console.log('failed to connect to DB getAllQueuedStudents');
     })
 }
 
 function clearStudentQueue() {
-  console.log('inside clear student queue mongodb')
+  console.log('inside clear student queue mongodb');
+  getDB()
+    .then( db => {
+      db.student-q.delete({}, (deleteErr, res) => {
+        if(deleteErr) {
+          console.log('error deleting queue');
+          db.close();
+        } else {
+          console.log(res);
+          db.close();
+          return res;
+        }
+      })
+    })
+    .catch( err => {
+      console.log('failed to connect to DB clearStudentQueue');
+    });
 }
 
-function clearTAQueue() {
-  console.log('inside clear TA queue mongodb');
+function clearTAQueue(TAid) {
+  console.log('inside clear TA queue mongodb', TAid);
+  console.log('this still deletes all the queue');
+  getDB()
+    .then( db => {
+      db.ta-q.delete({}, (deleteErr, res) => {
+        if(deleteErr) {
+          console.log('error deleting TA queue');
+          db.close();
+        } else {
+          console.log(res);
+          db.close();
+          return res;
+        }
+      })
+    })
+    .catch( err => {
+      console.log('failed to connect to DB clearTAQueue');
+    });
 }
 
 
